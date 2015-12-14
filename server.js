94c4de4b32a51ce7806f13aa481cdffe53a1e650
Mike Reed <mike.reed.vallejo@gmail.com>
2015-12-13 16:19:03 -0800

    two way communication twixt clients

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io');
+	
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+		
+		// io.emit  // send to all sockets
+		socket.broadcast.emit('message', message);  //send to all sockets but the sender.	
+	});
+	socket.emit('message', {
+		text: 'Welcome to my Chat App.'
+	});
 });
 
 http.listen(PORT, function () {
