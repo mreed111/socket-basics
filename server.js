b5d1b1e5b13b3fc3a0ad26d67737053c6ac7819c
Mike Reed <mike.reed.vallejo@gmail.com>
2015-12-15 16:51:25 -0800

    add disconnect event and message

--- server.js
+++ server.js
@@ -14,32 +14,38 @@ var clientInfo = {};
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 	
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+		
+		if ( typeof userData !== 'undefined' ) {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', systemMessage(userData.name + ' has left!'));
+		}
+	});
+	
 	socket.on('joinChatRoom', function (req) {
 		//
 		clientInfo[socket.id] = req;
 		//
 		socket.join(req.room);
-		socket.broadcast.to(req.room).emit('message', {
-			name: 'System',
-			text: req.name + ' has joined!',
-			timestamp: moment().valueOf()
-		});
+		socket.broadcast.to(req.room).emit('message', systemMessage(req.name + ' has joined!'));
 	});
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 		message.timestamp = moment().valueOf();
-		// io.emit  
-		// send to all sockets
-		//send to all sockets but the sender -- socket.broadcast.emit('message', message);
 		console.log('...emitting msg "'+message.text+'" from ' + message.name );
 		io.to(clientInfo[socket.id].room).emit('message', message);  	
 	});
-	socket.emit('message', {
-		name: 'System',
-		text: 'Welcome to my Chat App!',
-		timestamp: moment().valueOf()
-	});
+	socket.emit('message', systemMessage('Welcome to my Chat App!'));
 });
+
+function systemMessage (message) {
+	return {
+		name: 'System',
+		text: message,
+		timestamp: moment().valueOf()	
+	};
+}
 
 http.listen(PORT, function () {
 	console.log('Server listening on port ' + PORT);
